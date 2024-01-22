041b122852542651ba02c28034a094b66da4d534
potatoj1n <155697505+potatoj1n@users.noreply.github.com>
2024-01-22 15:35:56 +0900

    Update 문자열 내 마음대로 정렬하기.cpp

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int i;
 bool compare(string a, string b){
     if(a[i] == b[i]) 
-			return a < b;
-		return a[i]<b[i];
+	return a < b;
+    return a[i]<b[i];
 }
 
 vector<string> solution(vector<string> strings, int n) {
